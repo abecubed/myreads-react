4ae5082862f6193ff696857867c4315fc6657478
Trey Elder <Trey_Elder@carmax.com>
2025-07-08 16:00:36 -0400

    updated with tests

--- myreads/playwright/playwright.config.ts
+++ myreads/playwright/playwright.config.ts
@@ -16,17 +16,17 @@ export default defineConfig({
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
+  forbidOnly: false,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: 2,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://localhost:3000/',
+    baseURL: 'http://localhost:3001/',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -39,15 +39,15 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
 
     /* Test against mobile viewports. */
     // {
--- myreads/playwright/tests/bookshelf.spec.ts
+++ myreads/playwright/tests/bookshelf.spec.ts
@@ -3,6 +3,9 @@ import { test, expect } from '@playwright/test';
 test('Bookshelf has three headers', async ({ page }) => {
   await page.goto('');
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  //assert bookshelf options are visible
+  expect(page.getByRole('heading', {level: 1, name: 'MyReads'})).toBeVisible();
+  expect(page.getByRole('heading', { name: 'Currently Reading' })).toBeVisible();
+  expect(page.getByRole('heading', { name: 'Want to Read' })).toBeVisible();
+  await expect(page.getByRole('heading', { name: 'Read', exact: true })).toBeVisible();
 });
